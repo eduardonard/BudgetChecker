68b4543b2757ed7215bb8536664b0c5590f569d0
eduardonard <eduardomnardone@gmail.com>
2023-02-27 21:32:38 +0100

    ok

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 import os
-
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session, jsonify
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 import datetime
-
 from helpers import apology, login_required, lookup, usd
 
-# Configure application
 app = Flask(__name__)
 
 # Configure session to use filesystem (instead of signed cookies)
@@ -17,12 +14,29 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///progetto.db")
 
-@app.route("/", methods=["GET", "POST"])
-@login_required
+@app.route("/")
 def index():
+    return render_template("homepage.html")
+
+@app.route("/financeCalculators")
+def financeCalculators():
+    return render_template("calculators/financeCalculators.html")
+
+@app.route("/investmentsCalculators")
+def investmentsCalculators():
+    return render_template("investmentsCalculators.html")
+
+@app.route("/healthCalculators")
+def healthCalculators():
+    return render_template("healthCalculators.html")
+
+
+########### USER PAGES ##########
+@app.route("/home", methods=["GET", "POST"])
+@login_required
+def home():
     user_id = session["user_id"] 
     username = db.execute("SELECT username FROM users WHERE id = ?", user_id)[0]["username"]
     income_db = db.execute("SELECT amount, category, date, comment FROM transactions WHERE user_id = ? AND amount > 0 ORDER BY date DESC", user_id)
@@ -30,7 +44,7 @@ def index():
     incCategori = db.execute("SELECT category FROM categories WHERE user_id = ? AND type = 'income'",user_id)
     expCategori = db.execute("SELECT category FROM categories WHERE user_id = ? AND type = 'liability'",user_id)
     cash = db.execute("SELECT cash FROM users WHERE id = ?", user_id)[0]["cash"]
-    indes = render_template("index.html", income_db = income_db, liabilities_db = liabilities_db, cash = cash, incCategories=incCategori, expCategories = expCategori, username=username)
+    indes = render_template("user/index.html", income_db = income_db, liabilities_db = liabilities_db, cash = cash, incCategories=incCategori, expCategories = expCategori, username=username)
     if request.method == "GET":
         return indes
     elif request.method == "POST" and request.form.get("amount") != None:
@@ -54,7 +68,6 @@ def index():
         liabilities_db = db.execute("SELECT amount, category, date FROM transactions WHERE user_id = ? AND amount < 0", user_id)
     return redirect("/")
 
-
 @app.route("/settings", methods=["GET", "POST"])
 @login_required
 def settings():
@@ -76,21 +89,20 @@ def settings():
         return redirect("/settings")
     return redirect("/settings")
 
+
+######### LOGIN FUNCTIONS ########
 @app.route("/login", methods=["GET", "POST"])
 def login():
     session.clear()
     if request.method == "POST":
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return render_template("login.html",error="Invalid username and/or password")
+            return render_template("user/login.html",error="Invalid username and/or password")
         session["user_id"] = rows[0]["id"]
-        return redirect("/")
+        return redirect("/home")
     elif request.method == "GET":
-        return render_template("login.html")
+        return render_template("user/login.html")
 
-@app.route("/home")
-def home():
-    return render_template("homepage.html")
 
 @app.route("/logout")
 def logout():
@@ -102,38 +114,32 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
     if request.method == "GET":
-        return render_template("register.html")
+        return render_template("user/register.html")
     elif request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
         verify = request.form.get("confirmation")
-
         if not username:
-            return apology("Username required")
-
+            return render_template("user/register.html",error="Username required")
         if not password:
-            return apology("Password required")
-
+            return render_template("user/register.html",error="Password required")
         if not verify:
-            return apology("Password verification required")
-
+            return render_template("user/register.html",error="Verification required")
         if password != verify:
-            return apology("Passwords don't match, try again")
-
+            return render_template("user/register.html",error="Passwords don't match, try again")
         hash = generate_password_hash(password)
-
         try:
             newUser = db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
         except:
-            return apology("Username exists")
-
+            return render_template("user/register.html",error="User exists")
         session["user_id"] = newUser
+        return redirect("login")
 
-        return redirect("/")
 
 @app.errorhandler(404)
 def page_not_found(e):
